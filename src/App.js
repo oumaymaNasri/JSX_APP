f0a1fe471bdb031a4abf8a151c14079d0e2b8365
oumaymaNasri <76613383+oumaymaNasri@users.noreply.github.com>
2021-07-22 01:03:50 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import imageInSrc from './imageInSrc.jpg'
+import './style.css';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src={imageInSrc} alt='imageInSrc'/>
+          <br />
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
